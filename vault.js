5fd633505eb87a4cbf8c6557ac3fb64ef98eae0f
Chaz-C <chazcastro@Chazs-MacBook-Pro.local>
2016-11-22 20:11:47 -1000

    finished

--- vault.js
+++ vault.js
@@ -1,4 +1,24 @@
 'use strict';
 module.exports = function() {
-  
+
+  var treasure = {
+  };
+
+  function _setValue(key, value) {
+    treasure[key] = value;
+    return treasure;
+  }
+
+  function _getValue(key) {
+    if ( key in treasure ) {
+      return treasure[key];
+    } else {
+      return null;
+    }
+  }
+
+  return {
+    setValue: _setValue,
+    getValue: _getValue
+  };
 };
